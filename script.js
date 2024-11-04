05554ca3599a044ac97c391d46f2042b553ca1c9
Jitendra-singh-cell <jitendrasingh9x221@gmail.com>
2024-11-04 22:14:44 +0500

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+// Focus next input on entering a number, or focus previous input on backspace
+codes.forEach((code, index) => {
+    code.addEventListener('keydown', (e) => {
+        if (e.key >= '0' && e.key <= '9') {
+            // Move to the next input
+            codes[index].value = ''; // Reset current value
+            setTimeout(() => {
+                if (index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            }, 10);
+        } else if (e.key === 'Backspace') {
+            // Move to the previous input on backspace
+            setTimeout(() => {
+                if (index > 0) {
+                    codes[index - 1].focus();
+                }
+            }, 10);
+        }
+    });
+});
